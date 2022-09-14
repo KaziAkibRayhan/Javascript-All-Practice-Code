122cc11b6857948e6559b5a4d4aa5ea955f1906f
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-09-14 23:58:16 +0600

    Variable declaration and set values

---
+++ variable/declare-variable.js
@@ -0,0 +1,18 @@
+// Number Type Variable
+var waterBottlePrice = 176;
+// Set a value 
+waterBottlePrice = 180;
+
+// String Type Variable
+var waterBottleColor = 'red';
+// Boolean Type
+var isRainny = false;
+
+// let && const  Use 
+
+let jhonAges = 13; /* Change Able */
+jhonAges = 14;
+const pi = 3.1416; /* Not Change Able */
+// pi = 23;
+
+console.log(waterBottleColor, waterBottlePrice, isRainny, jhonAges, pi);
