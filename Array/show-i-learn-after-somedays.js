d2368048a7cd729e443ee9451f4269a18a93dda6
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-09-15 23:44:47 +0600

    find odd even by array

---
+++ Array/show-i-learn-after-somedays.js
@@ -0,0 +1,5 @@
+let iHaveLearn = ["HTML", "CSS", "JAVASCRIPT", "TAILWIND", "BOOTSTRAP"];
+for (let i = 0; i < iHaveLearn.length; i++) {
+    const element = iHaveLearn[i];
+    console.log(element);
+}
