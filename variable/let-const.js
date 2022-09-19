5b7e95b400df883503d400505c4f5ab8c80a859a
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-09-19 22:20:40 +0600

    let const update

--- variable/let-const.js
+++ variable/let-const.js
@@ -1,4 +1,8 @@
 let myBudgetPen = 120;
-
-myBudgetPen = 130;
+myBudgetPen = 130; // reasigned variable
 console.log(myBudgetPen);
+
+const myName = 'Akib Rayhan';
+console.log(myName);
+// myName = 'Md. Akib Rayhan'; // not able reasigned variable....
+// console.log(myBudgetPen);
